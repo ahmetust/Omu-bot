0ed6a616f2af763ffe120fcb308092417bad0928
Emre Demirel <fed3224@gmail.com>
2024-07-27 16:53:50 +0300

    some problems are resolved

--- Flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_speech
+import flutter_tts
+import path_provider_foundation
+import speech_to_text_macos
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSpeechRecognitionPlugin.register(with: registry.registrar(forPlugin: "FlutterSpeechRecognitionPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
